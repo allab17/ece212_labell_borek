95e559b6a87cf3c546520f9670027cebf6cc0c11
allab17 <labella@lafayette.edu>
2022-04-18 14:15:12 -0400

    pushing finished lab 10, named lab09.2, since lab 10 is just a continuation of lab 9. PLEASE IGNORE THE ORIGINAL lab09

--- lab09/rtl/control_fsm.sv
+++ lab09/rtl/control_fsm.sv
@@ -21,7 +21,7 @@
 
 
 module control_fsm(input logic clk, rst, [5:0] op,
-                   output logic IorD, ALUSrcA, [1:0] ALUSrcB, [1:0] ALUOp, [1:0] PCSource, IRWrite, PCWrite, RegDst, MemToReg, RegWrite, MemWrite, Branch 
+                   output logic iord, alusrca, [1:0] alusrcb, [1:0] aluop, [1:0] pcsource, irwrite, pcwrite, regdst, memtoreg, regwrite, memwrite, branch 
             );
     
     typedef enum logic [3:0] {
@@ -48,31 +48,31 @@ module control_fsm(input logic clk, rst, [5:0] op,
     end
     
     always_comb begin
-    IorD = 0;
-    ALUSrcA = 0;
-    ALUSrcB = 0;
-    ALUOp = 0;
-    PCSource = 0;
-    IRWrite = 0;
-    PCWrite = 0;
+    iord = 0;
+    alusrca = 0;
+    alusrcb = 0;
+    aluop = 0;
+    pcsource = 0;
+    irwrite = 0;
+    pcwrite = 0;
         case (state)
         FETCH:
         begin
-            IorD = 0;
-            ALUSrcA = 0;
-            ALUSrcB = 01;
-            ALUOp = 00;
-            PCSource = 00;
-            IRWrite = 1;
-            PCWrite = 1;
+            iord = 0;
+            alusrca = 0;
+            alusrcb = 01;
+            aluop = 00;
+            pcsource = 00;
+            irwrite = 1;
+            pcwrite = 1;
             next = DECODE;
         end
         
         DECODE:
         begin
-            ALUSrcA = 0;
-            ALUSrcB = 2'b11;
-            ALUOp = 2'b00;
+            alusrca = 0;
+            alusrcb = 2'b11;
+            aluop = 2'b00;
             if (op == 6'b101011 || op == 6'b100011) next = MEMADR;
             if (op == 6'd0) next = RTYPEEX;
             if (op == 6'b000100) next = BEQEX;
@@ -82,92 +82,92 @@ module control_fsm(input logic clk, rst, [5:0] op,
         
         MEMADR:
         begin
-            ALUSrcA = 1;
-            ALUSrcB = 2'b10;
-            ALUOp = 2'b00;
+            alusrca = 1;
+            alusrcb = 2'b10;
+            aluop = 2'b00;
             if (op == 6'b101011) next = MEMWR;
             if (op == 6'b100011) next = MEMRD;
         end
         
         MEMRD:
         begin
-            IorD = 1;
+            iord = 1;
             next = MEMWB;
         end
         
         MEMWB:
         begin
-            RegDst = 0;
-            MemToReg = 1;
-            RegWrite = 1;
+            regdst = 0;
+            memtoreg = 1;
+            regwrite = 1;
             next = FETCH;
         end
         
         MEMWR:
         begin
-            IorD = 1;
-            MemWrite = 1;
+            iord = 1;
+            memwrite = 1;
             next = FETCH;
         end
         
         RTYPEEX:
         begin
-            ALUSrcA = 1;
-            ALUSrcB = 2'b00;
-            ALUOp = 2'b10;
+            alusrca = 1;
+            alusrcb = 2'b00;
+            aluop = 2'b10;
             next = RTYPEWB;
         end
         
         RTYPEWB:
         begin
-            RegDst = 1;
-            MemToReg = 0;
-            RegWrite = 1;
+            regdst = 1;
+            memtoreg = 0;
+            regwrite = 1;
             next = FETCH;
         end
         
         BEQEX:
         begin
-            ALUSrcA = 1;
-            ALUSrcB = 2'b00;
-            ALUOp = 2'b01;
-            PCSource = 2'b01;
-            Branch = 1;
+            alusrca = 1;
+            alusrcb = 2'b00;
+            aluop = 2'b01;
+            pcsource = 2'b01;
+            branch = 1;
             next = FETCH;
         end
         
         ADDIEX:
         begin
-            ALUSrcA = 1;
-            ALUSrcB = 2'b10;
-            ALUOp = 2'b00;
+            alusrca = 1;
+            alusrcb = 2'b10;
+            aluop = 2'b00;
             next = ADDIWB;
         end
         
         ADDIWB:
         begin
-            RegDst = 0;
-            MemToReg = 0;
-            RegWrite = 1;
+            regdst = 0;
+            memtoreg = 0;
+            regwrite = 1;
             next = FETCH;
         end
         
         JEX:
         begin
-           PCSource = 2'b10;
-           PCWrite = 1; 
+           pcsource = 2'b10;
+           pcwrite = 1; 
            next = FETCH;
         end
         
         default:
         begin
-            IorD = 0;
-            ALUSrcA = 0;
-            ALUSrcB = 0;
-            ALUOp = 0;
-            PCSource = 0;
-            IRWrite = 0;
-            PCWrite = 0;
+            iord = 0;
+            alusrca = 0;
+            alusrcb = 0;
+            aluop = 0;
+            pcsource = 0;
+            irwrite = 0;
+            pcwrite = 0;
         end
 
         endcase
